76b4db3eff5343e14b7a5a2a84a4e40a36b2d625
mrsteveson <patrick.steveson727@gmail.com>
2019-02-26 17:42:24 -0600

    attempted challenge 2 in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -61,12 +61,23 @@ wordmathproblem();
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+// const counter = () => {
+//   Return a function that when invoked increments and returns a counter variable.
+// };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+let counter = 0;
+
+let add = (function() {
+  return function () {counter +=1; return counter}
+})();
+
+add();
+add();
+
+console.log(counter);
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {

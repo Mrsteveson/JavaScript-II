eadc920276406f212ea1395e841a481228c827b5
mrsteveson <patrick.steveson727@gmail.com>
2019-02-26 15:16:31 -0600

    done with the nonstretch callback exercises

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -25,26 +25,76 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
+// function getLength(arr, cb) {
+//   // getLength passes the length of the array into the callback.
+// }
+
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length)
+});
+
+
+// function last(arr, cb) {
+//   // last passes the last item of the array into the callback.
+// }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(length){
+  console.log(length)
+});
+
+// function sumNums(x, y, cb) {
+//   // sumNums adds two numbers (x, y) and passes the result to the callback.
+// }
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(items, function(add){
+  console.log(add)
+});
+
+
+
+// function multiplyNums(x, y, cb) {
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+// }
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
+multiplyNums(items, function(multiply){
+  console.log(multiply)
+});
+
+
+// function contains(item, list, cb) {
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+// }
+
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i <list.length; i++){
+    if(list[i] === item){
+      return cb(true);
+    } else {
+      return cb(false);
+    }
+  }
 }
+
+contains(item, list, function(trueorfalse){
+  console.log(trueorfalse)
+});
 
 /* STRETCH PROBLEM */
 

1c16771eec30aba1c5d94ed0bfa3d0d984865b7f
mrsteveson <patrick.steveson727@gmail.com>
2019-02-26 13:59:09 -0600

    done with the function conversion practice

--- assignments/stretch-function-conversion.js
+++ assignments/stretch-function-conversion.js
@@ -14,9 +14,7 @@ const anotherfunction = (param) => param;
 // };
 // add(1,2);
 
-const add = (param1, param2) => {
-    return param1 + param2;
-};
+const add = (param1, param2) => (param1 + param2);
 
 console.log(add(1, 2));
 
@@ -25,9 +23,7 @@ console.log(add(1, 2));
 // };
 // subtract(1,2);
 
-const subtract = (param1, param2) => {
-    return param1 - param2;
-};
+const subtract = (param1, param2) => (param1 - param2);
 
 console.log(subtract(1, 2));
 

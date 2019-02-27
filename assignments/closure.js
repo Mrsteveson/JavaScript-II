3e55ff82efdd795820445ca0c3e52f4ab56c43d3
mrsteveson <patrick.steveson727@gmail.com>
2019-02-26 21:00:49 -0600

    worked on closure stretch goals in afterhours

--- assignments/closure.js
+++ assignments/closure.js
@@ -55,6 +55,16 @@ wordmathproblem();
 // Will work on more complex ones using things other than strings later, starting to understand the concept though. :D
 
 
+// Another example.
+
+let add = (function() {
+  return function () {counter +=1; return counter}
+})();
+
+add();
+add();
+
+console.log(counter);
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -68,20 +78,52 @@ wordmathproblem();
 // newCounter(); // 1
 // newCounter(); // 2
 
-let counter = 0;
+const counter = () => {
+  let counterVar = 0;
 
-let add = (function() {
-  return function () {counter +=1; return counter}
-})();
+  return function() {
+    counterVar++
+    console.log(counterVar);
 
-add();
-add();
+  }
 
-console.log(counter);
+}
+
+const newCounter = counter();
+
+newCounter();
+newCounter();
+
+
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  
+  let counter = 0;
+  return {
+
+    increment: function() {
+      counter++;
+      return counter;
+
+    },
+
+    decrement: function () {
+      counter--;
+      return counter;
+
+    }
+
+  }
+
 };
+
+const counter = counterFactory ();
+counterFactory();
+
+console.log(newCounter.increment());
+console.log(newCounter.decrement());
